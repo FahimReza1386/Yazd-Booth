ddd9ddafbb23ae50c0d84da59949dc8e1cd21a26
FahimReza <FahimReza202@outlook.com>
2024-08-09 17:32:17 +0330

    Create The Login and Logout and About

--- Website/Store/urls.py
+++ Website/Store/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.Home, name='Home'),
+    path('about/', views.About, name='About'),
+    path('login/', views.Login, name='Login'),
+    path('logout/', views.Logout, name='Logout'),
 ]
--- Website/Store/views.py
+++ Website/Store/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,redirect
 from .models import *
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 # Create your views here.
@@ -10,4 +12,28 @@ def Home(request):
     booth= Booth.objects.all()
     category = Category.objects.all()
     return render(request=request , template_name='Home.html' , context={'Product':products ,'Booth':booth , 'category':category})
+
+
+def About(request):
+    return render(request=request , template_name='About.html' , context={})
+
+def Login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'تبریک ! با موفقیت به حساب خود وارد شدید ...')
+            return redirect('/')
+        else:
+            messages.error(request, 'نام کاربری یا رمز عبور معتبر نیست ...')
+            return redirect('Login')
+    else:
+        return render(request=request, template_name='Login.html', context={})
+
+def Logout(request):
+    logout(request)
+    messages.success(request, 'با موفقیت از حساب خود خارج شدید ...')
+    return redirect('/')
 
