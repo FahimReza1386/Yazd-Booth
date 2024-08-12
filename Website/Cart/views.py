c8de887a4a20d32d7548d068249b9d2c1a7487de
FahimReza <FahimReza202@outlook.com>
2024-08-13 00:44:40 +0330

    Create The Quantity in Cart and Save The Chaneg Product Page

--- Website/Cart/views.py
+++ Website/Cart/views.py
@@ -1,12 +1,34 @@
-from django.shortcuts import render
-
-
+from django.shortcuts import render , get_object_or_404
+from .cart import Cart
+from Store.models import Product
+from django.http import JsonResponse
 # Create your views here.
 def Cart_Summary(request):
     return render(request=request, template_name='Cart_Summary.html' , context={})
 
 def Cart_Add(request):
-    pass
+
+    # Get The Cart
+    cart = Cart(request)
+
+    if request.POST.get('action') == 'post':
+        #Get stuff
+        product_id=int(request.POST.get('product_id'))
+
+        # Lookup product in DB
+        product = get_object_or_404(Product, id=product_id)
+
+        # Save To Session
+        cart.add(product=product)
+
+        # Get The Cart Quantity
+        cart_quantity=cart.__len__()
+
+
+        # Return response
+        response = JsonResponse({'qty ' : cart_quantity})
+        return response
+
 
 def Cart_Delete(request):
     pass
