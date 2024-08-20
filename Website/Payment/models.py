0fcf1791e545404713b0b72b5638ef2386a2f013
FahimReza <FahimReza202@outlook.com>
2024-08-20 09:49:54 +0330

    Create The Order Model And OrderItem Model

--- Website/Payment/models.py
+++ Website/Payment/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from Store.models import Product
 
 # Create your models here.
 
@@ -24,3 +24,32 @@ class ShippingAddress(models.Model):
 
     def __str__(self):
         return f' Shipping Address - {str(self.user)}'
+
+
+
+# Create Order Model
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE , null=True , blank=True)
+    full_name = models.CharField(max_length=250)
+    email = models.EmailField(max_length=255)
+    shipping_address = models.CharField(max_length=15000)
+    amount_paid = models.DecimalField(max_digits=10, decimal_places=0)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f'{str(self.id)}'
+
+
+
+# Create Order Item Model
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True , blank=True)
+
+    quantity = models.PositiveBigIntegerField(default=1)
+    price = models.DecimalField(max_digits=10, decimal_places=0)
+
+    def __str__(self):
+        return f'{str(self.id)}'
