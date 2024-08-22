93ac8398da3f65cbaa0acfb876aad6f8aa2ce50a
FahimReza <FahimReza202@outlook.com>
2024-08-22 12:41:53 +0330

    Create Date_shipped in DataBase Order

--- Website/Payment/models.py
+++ Website/Payment/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 from Store.models import Product
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save , pre_save
+from django.dispatch import receiver
+import datetime
 
 # Create your models here.
 
@@ -37,12 +39,20 @@ class Order(models.Model):
     amount_paid = models.DecimalField(max_digits=10, decimal_places=0)
     date_ordered = models.DateTimeField(auto_now_add=True)
     shipped = models.BooleanField(default=False)
+    date_shipped = models.DateTimeField(blank=True, null=True)
 
 
     def __str__(self):
         return f'{str(self.id)} - {self.full_name}'
 
-
+# Auto Add Shipping_Date
+@receiver(pre_save, sender=Order)
+def set_shipped_date_on_update(sender, instance, **kwargs):
+    if instance.pk:
+        now = datetime.datetime.now()
+        obj = sender._default_manager.get(pk=instance.pk)
+        if instance.shipped and not obj.shipped:
+            instance.date_shipped = now
 
 # Create Order Item Model
 class OrderItem(models.Model):
