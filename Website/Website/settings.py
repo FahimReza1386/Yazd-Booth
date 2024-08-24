ac10564cda8a489a145658e2bc722438e66d3328
FahimReza <FahimReza202@outlook.com>
2024-08-24 12:02:29 +0330

    Cloud DtataBase In Postgresql

--- Website/Website/settings.py
+++ Website/Website/settings.py
@@ -12,10 +12,15 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+# Load our environmental variables
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -75,14 +80,20 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Website.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'yazdbooth',
+        'USER': 'postgres',
+        'PASSWORD': 'Fahim2684',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
